bc3d5c7917e896df297b50526bf7e0756ac07945
tcnksm <nsd22843@gmail.com>
2014-12-25 19:05:39 +0900

    Add script to interact with DNSimple

---
+++ scripts/dnsimple/record.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Create A(ddress) Record on DNSimple.
+# DNSimple API document is here, http://developer.dnsimple.com/
+
+# POST contents 
+JSON="
+{
+    \"record\": {
+        \"name\": \"\",
+        \"record_type\": \"A\",
+        \"content\": \"${BLOG_IP}\",
+        \"ttl\": 3600
+    }
+}
+"
+
+# Create record
+curl  -H "X-DNSimple-Token: ${DNSIMPLE_EMAIL}:${DNSIMPLE_TOKEN}" \
+      -H "Accept: application/json" \
+      -H "Content-Type: application/json" \
+      -X POST \
+      -d "${JSON}" \
+      https://api.dnsimple.com/v1/domains/deeeet.com/records
+
+
+# Check the DNS record configuration
+dig @ns1.dnsimple.com deeeet.com A

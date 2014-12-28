71c53ea2ce449f0730fcd8f8931c70e6b2a9d126
tcnksm <nsd22843@gmail.com>
2014-12-28 15:57:41 +0900

    Add new load-balancer container

---
+++ dockerfiles/lb/1.0/usr/local/bin/confd-watch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# We can access etcd by 172.17.42.1 from docker container on CoreOS. 
+export ETCD_PORT=${ETCD_PORT:-4001}
+export HOST_IP=${HOST_IP:-172.17.42.1}
+export ETCD=$HOST_IP:$ETCD_PORT
+
+echo "[nginx] booting container. ETCD: $ETCD"
+
+# Try to make initial configuration every 5 seconds until successful
+until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/nginx.toml; do
+    echo "[nginx] waiting for confd to create initial nginx configuration"
+    sleep 5
+done
+
+# Put a continual polling `confd` process into the background to watch
+# for changes every 10 seconds
+confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/nginx.toml &
+echo "[nginx] confd is now monitoring etcd for changes..."
+
+# Start the Nginx service using the generated config
+echo "[nginx] starting nginx service..."
+service nginx start
+
+# Follow the logs to allow the script to continue running
+tail -f /var/log/nginx/*.log
---
+++ dockerfiles/lb/README.md
@@ -0,0 +1,14 @@
+Dockerfile deeeet.com Load-Balancer
+====
+
+Dockerfile for [deeeet.com](http://deeeet.com/)'s laod-balancer container. The load-balancer use [confd](https://github.com/kelseyhightower/confd) and watch changes on [etcd](https://github.com/coreos/etcd) and re-render nginx.conf file once changes happened. 
+
+## Tags
+
+`tcnksm/deeeet-com-lb` image supports below tags. Link is its `Dockerfile`. 
+
+- [`latest`,`1.0` (1.0/Dockerfile)](https://github.com/tcnksm/deeeet.com/blob/master/dockerfiles/lb/1.0/Dockerfile)
+
+## Author
+
+[tcnksm](https://github.com/tcnksm)

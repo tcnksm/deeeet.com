40913d28af3417117c7472b77d60b52469a9ea5f
tcnksm <nsd22843@gmail.com>
2015-01-26 23:04:50 +0900

    Change name

--- terraform/dnsimple.tf
+++ terraform/dnsimple.tf
@@ -1,6 +1,6 @@
 # Add a record to the domain
 resource "dnsimple_record" "deeeet-com" {
-  domain = "coreos-deeeet.com"
+  domain = "deeeet.com"
   name = "@"
   value = "${digitalocean_droplet.deeeet-com-lb1.ipv4_address}"
   type = "A"

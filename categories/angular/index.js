2f907c94c4bf48ecc3e933c346a88747250c8748
tcnksm <nsd22843@gmail.com>
2015-10-05 15:20:30 +0900

    Updating site

---
+++ categories/angular/index.js
@@ -0,0 +1,8 @@
+  <section class="index"> 
+    <h2><a href="http://deeeet.com/categories/angular/index.js/">Angular.Js</a></h2>
+    
+    <div class="meta">
+      Written By <address></address> &mdash;
+      <time pubdate datetime="2013-12-23" title="2013-12-23">December 12, 2013</time>
+    </div>
+  </section>

adfa43abcd37fefb5da4c882d6e6f5b8465e1b85
tcnksm <nsd22843@gmail.com>
2015-11-19 02:54:37 +0900

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "----> Remove draft"
+cd public && git ls-files --others --exclude-standard | xargs rm
+
+echo "----> Run Hugo"
+hugo
+
+echo "----> Update atom"
+cd public && mv index.xml writing/atom.xml

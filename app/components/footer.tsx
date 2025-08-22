fbd9673788a9495a7e6e1d09eb2e879841c0bd7c
tcnksm <taichi@hey.com>
2025-08-22 15:13:38 +0900

    Update profile and change style

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -52,6 +52,17 @@ export default function Footer() {
             <p className="ml-2 h-7">Twitter</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://instagram.com/tcnksm"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">Instagram</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} Taichi Nakashima
--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,42 @@ export default function Page() {
         Taichi Nakashima
       </h1>
       <p className="mb-4">
-        {`This is Taichi Nakashima (@deeeet)'s personal blog. 
-        I'm a software engineer currently working at Mercari and leading the internal platform group as tech lead. 
-        You can find me on Twitter or GitHub.`}
+        I'm Taichi Nakashima, known as{" "}
+        <a href="https://x.com/deeeet" className="about_link">
+          deeeet
+        </a>{" "}
+        on the social media, a Software Engineer living in Tokyo, Japan 🗼 I'm
+        currently a VP of Engineering at Mercari, leading global expasion team.
+        Prior to this, I was a Director of Engineering at Mercari, leading{" "}
+        <a
+          href="https://speakerdeck.com/tcnksm/platform-engineering-at-mercari-platform-engineering-kaigi-2024"
+          className="about_link"
+        >
+          Platform Engineering team
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://speakerdeck.com/tcnksm/sre-practices-in-mercari-microservices"
+          className="about_link"
+        >
+          SRE team
+        </a>{" "}
+        .
+      </p>
+      <p className="mb-4">
+        I'm a camper 🏕️ and enjoy camping around Japan nature. See some of my
+        camping photos on{" "}
+        <a href="https://instagram.com/tcnksm" className="about_link">
+          Instagram
+        </a>{" "}
+        or check my articles about favorite camp gears from{" "}
+        <a
+          href="https://note.com/deeeet/n/n01be8131e317"
+          className="about_link"
+        >
+          here
+        </a>
+        .
       </p>
     </section>
   );

e9207b7bd90770e0708fbf89a8289d188ccd2e44
tcnksm <taichi@hey.com>
2025-08-22 15:23:41 +0900

    Add icon

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,33 +1,24 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+import {
+  TwitterIcon,
+  GitHubIcon,
+  InstagramIcon,
+  RSSIcon,
+  LinkedInIcon,
+} from "./icons";
 
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul className="font-sm mt-8 flex flex-row space-x-4 text-neutral-600 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
             href="/rss"
+            aria-label="RSS Feed"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">RSS</p>
+            <RSSIcon />
           </a>
         </li>
         <li>
@@ -36,9 +27,9 @@ export default function Footer() {
             rel="noopener noreferrer"
             target="_blank"
             href="https://github.com/tcnksm"
+            aria-label="GitHub Profile"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">Github</p>
+            <GitHubIcon />
           </a>
         </li>
         <li>
@@ -47,9 +38,9 @@ export default function Footer() {
             rel="noopener noreferrer"
             target="_blank"
             href="https://x.com/deeeet"
+            aria-label="Twitter Profile"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">Twitter</p>
+            <TwitterIcon />
           </a>
         </li>
         <li>
@@ -58,9 +49,20 @@ export default function Footer() {
             rel="noopener noreferrer"
             target="_blank"
             href="https://instagram.com/tcnksm"
+            aria-label="Instagram Profile"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">Instagram</p>
+            <InstagramIcon />
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/taichi-nakashima-77746521"
+            aria-label="LinkedIn Profile"
+          >
+            <LinkedInIcon />
           </a>
         </li>
       </ul>
